1ac122dcee5a8d320a70c392eb6c5f9aecfb279d
AKarakehayova <alexzk95@gmail.com>
2016-01-27 23:30:29 +0200

    upgraded solution

--- solutions/07.rb
+++ solutions/07.rb
@@ -1,191 +1,253 @@
 module LazyMode
-  def self.create_file(name, &block)
-    object = File.new(name)
-    object.instance_eval &block
-    object
+  def self.create_file(file_name, &block)
+    File::NoteDSL.create(file_name, &block)
   end
 
   class Date
-    def initialize(date)
-      @date = date
-      @day = date.split('-')[2].to_i
-      @month = date.split('-')[1].to_i
-      @year = date.split('-')[0].to_i
-    end
+    class Diff
+      def initialize(first, second)
+        @first, @second = first, second
+      end
 
-    def year
-      @year
-    end
-
-    def month
-      @month
-    end
-
-    def day
-      @day
-    end
-
-    def add_day
-      if @day == 30 and @month == 12
-        @day, @month = 1, 1
-        @year += 1
-      elsif @day == 30
-        @day, @month = 1, @month + 1
-      else @day += 1
+      def days
+        @first.to_days - @second.to_days
       end
     end
 
+    attr_reader :year, :month, :day
+
+    def initialize(date)
+      @year, @month, @day = date.split('-').map(&:to_i)
+      @date = date
+    end
+
+    def add_days(days)
+      sum_days = to_days + days
+      day = (sum_days % 360) % 30
+      month = 1 + (sum_days % 360) / 30
+      year = sum_days / 360
+      Date.new(sprintf('%.4d-%.2d-%.2d', year, month, day))
+    end
+
+    def ==(date)
+      (self - date).days == 0
+    end
+
+    def -(date)
+      Diff.new(self, date)
+    end
+
+    def to_days
+      year * 360 + (month - 1) * 30 + day
+    end
+
     def to_s
-      @date.split('-')[2].insert(0, '0') if @date.split('-')[2].length < 2
-      @date.split('-')[1].insert(0, '0') if @date.split('-')[1].length < 2
-      @date.split('-')[0].insert(0, '0') if @date.split('-')[0].length < 4
       @date
     end
   end
 
-  class Note
-    attr_accessor :file_name, :body, :status, :header, :date, :period
-    def initialize(header, *tags, status, body)
-      @header = header
-      @body = body
-      @status = status
-      @tags = *tags.flatten
-      @file_name = ''
-      @period = 0
-    end
+  class NoteWithDate
+    attr_reader :file_name, :header, :tags, :status, :body, :date
 
-    def copy(date)
-      object = Note.new(@header, @tags, @status, @body)
-      object.date = date
-      object
-    end
-
-    def tags
-      @tags
+    def initialize(note, date)
+      @file_name = note.file_name
+      @header = note.header
+      @tags = note.tags
+      @status = note.status
+      @body = note.body
+      @date = date
     end
   end
 
-  class MainNote
-    attr_accessor :date, :all_notes
-    def initialize(header, *tags)
-      @header = header
-      @tags = *tags.flatten
-      @status = :topostpone
-      @period = 0
-    end
+  class Note
+    class Schedule
+      def initialize(date)
+        @date = date
+      end
 
-    def scheduled(period)
-      date = period.split('+')
-      @date = Date.new(date[0].chomp)
-      @period = define_period(date) if date.size == 2
-    end
-
-    def note(header, *tags, &block)
-      object = MainNote.new(header, *tags)
-      object.all_notes = @all_notes
-      object.instance_eval &block if block_given?
-      note = object.return_note
-      note.file_name = @name
-      @all_notes.push(note)
-    end
-
-    def define_period(date)
-      case date[1].chars.last
-      when 'w' then date[1].chars.first.to_i * 7
-      when 'd' then date[1].chars.first.to_i
-      when 'm' then date[1].chars.first.to_i * 30
+      def scheduled_for?(date)
+        @date == date
       end
     end
 
-    def status(status)
-      @status = status
+    class NullSchedule
+      def scheduled_for?(_)
+        false
+      end
     end
 
-    def body(body)
-      @body = body
+    class DailySchedule
+      def initialize(start, repeat)
+        @start_date = start
+        @repetition = repeat
+      end
+
+      def scheduled_for?(date)
+        since_start = (date - @start_date).days
+        if since_start < 0
+          false
+        else since_start % @repetition == 0
+        end
+      end
+
+      def self.from_string(string)
+        new(Date.new(string[0..9]), repeating_days(string))
+      end
+
+      private
+
+      def self.repeating_days(string)
+        repeater = string[11..-1]
+        case repeater
+        when /d$/ then 1  * repeater[/\d+/].to_i
+        when /w$/ then 7  * repeater[/\d+/].to_i
+        when /m$/ then 30 * repeater[/\d+/].to_i
+        end
+      end
     end
 
-    def return_note
-      object = Note.new(@header, @tags, @status, @body)
-      object.date = @date
-      object.period = @period
-      object
+    attr_reader :file_name, :header, :tags
+    attr_accessor :sub_notes, :status, :body, :schedule
+
+    def initialize(file_name, header, tags)
+      @file_name = file_name
+      @header = header
+      @tags = tags
+    end
+
+    def flatten_sub_notes
+      @sub_notes.flat_map { |sub_note| [sub_note] + sub_note.flatten_sub_notes }
+    end
+
+    def scheduled_for?(date)
+      @schedule.scheduled_for?(date)
+    end
+
+    def with_date(date)
+      NoteWithDate.new(self, date)
+    end
+
+    class NoteDSL
+      def initialize(file_name, header, tags)
+        @file_name = file_name
+        @header = header
+        @tags = tags
+        @sub_notes = []
+      end
+
+      def status(status)
+        @status = status
+      end
+
+      def body(body)
+        @body = body
+      end
+
+      def scheduled(date)
+        @schedule = if date[/[dwm]$/]
+                      DailySchedule.from_string(date)
+                    else
+                      Schedule.new(Date.new(date))
+                    end
+      end
+
+      def note(header, *tags, &block)
+        @sub_notes << self.class.create(@file_name, header, tags, &block)
+      end
+
+      def to_note
+        Note.new(@file_name, @header, @tags).tap do |note|
+          note.status = @status || :topostpone
+          note.body = @body || ''
+          note.schedule = @schedule || NullSchedule.new
+          note.sub_notes = @sub_notes
+        end
+      end
+
+      def self.create(file_name, header, tags, &block)
+        new_note = new(file_name, header, tags)
+        new_note.instance_eval(&block)
+        new_note.to_note
+      end
+    end
+  end
+
+  class FilteredNotes
+    attr_reader :notes
+
+    def initialize(notes)
+      @notes = notes
+    end
+
+    def where(tag: nil, status: nil, text: nil)
+      filtered_notes = notes.dup
+      filter_by_tag(tag, filtered_notes) if tag
+      filter_by_status(status, filtered_notes) if status
+      filter_by_text(text, filtered_notes) if text
+      FilteredNotes.new(filtered_notes)
+    end
+
+    private
+    def filter_by_tag(tag, filtered_notes)
+      filtered_notes.select! { |note| note.tags.include?(tag) }
+    end
+
+    def filter_by_status(status, filtered_notes)
+      filtered_notes.select! { |note| note.status == status }
+    end
+
+    def filter_by_text(regex, filtered_notes)
+      filtered_notes.select! do |note|
+        note.header[regex] || note.body[regex]
+      end
     end
   end
 
   class File
-    attr_accessor :all_notes
-    attr_reader :name
-    def initialize(name)
-      @name = name
-      @all_notes = []
-    end
+    attr_reader :name, :notes
 
-    def note(header, *tags, &block)
-      object = MainNote.new(header, *tags)
-      object.all_notes = @all_notes
-      object.instance_eval &block if block_given?
-      note = object.return_note
-      note.file_name = @name
-      @all_notes.push(note)
-    end
-
-    def notes
-      @all_notes
+    def initialize(name, notes)
+      @name, @notes = name, notes
     end
 
     def daily_agenda(date)
-      DailyAgenda.new(date, @all_notes)
+      filtered_notes = flatten_notes.select do |note|
+        note.scheduled_for?(date)
+      end
+      filtered_notes.map! { |note| note.with_date(date) }
+      FilteredNotes.new(filtered_notes)
     end
 
     def weekly_agenda(date)
-      WeeklyAgenda.new(date, @all_notes)
-    end
-
-    def where
-    end
-  end
-
-  class Agenda
-    def initialize(date, all_notes)
-      @date = date
-      @all_notes = all_notes
-      @notes = []
-    end
-
-    def notes
-      @all_notes.each { |note| check_dates(note.date, note) }
-      @notes
-    end
-
-    def date_difference(date)
-      year = (@date.year - date.year) * 360
-      month = (@date.month - date.month) * 30
-      day = (@date.day - date.day)
-      year + month + day
-    end
-
-    def check_dates(date, note)
-      if (note.period != 0) and date_difference(date) % note.period == 0
-        @notes.push(note.copy(@date))
+      filtered_notes = 0.upto(6).flat_map do |days|
+        daily_agenda(date.add_days(days)).notes
       end
+      FilteredNotes.new(filtered_notes)
     end
-  end
 
-  class DailyAgenda < Agenda
-    def notes
-      @all_notes.each { |note| check_dates(note.date, note) }
-      @notes
+    def flatten_notes
+      notes + notes.flat_map { |note| note.flatten_sub_notes }
     end
-  end
 
-  class WeeklyAgenda < Agenda
-    def notes
-      (0...7).each do
-        @notes += DailyAgenda.new(@date, @all_notes).notes
-        @date = @date.add_day
+    class NoteDSL
+      def initialize(file_name)
+        @file_name = file_name
+        @notes = []
       end
-      @notes
+
+      def note(header, *tags, &block)
+        @notes << Note::NoteDSL.create(@file_name, header, tags, &block)
+      end
+
+      def to_file
+        File.new(@file_name, @notes)
+      end
+
+      def self.create(file_name, &block)
+        new_note = new(file_name)
+        new_note.instance_eval(&block)
+        new_note.to_file
+      end
     end
   end
 end
